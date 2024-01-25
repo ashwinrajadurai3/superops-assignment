5da305dcd9aa5c6267c00f8d418e4074249dfb4b
Ashwin Rajadurai <63445141+ashwinrajadurai3@users.noreply.github.com>
2024-01-25 11:00:31 +0530

    Update user_data.sh to distinct request

--- web-app/modules/asg/user_data.sh
+++ web-app/modules/asg/user_data.sh
@@ -2,6 +2,6 @@
 sudo apt update -y
 sudo apt-get install nginx -y
 sudo cp /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html.bkp
-echo "HelloWorld!!!" | sudo tee /var/www/html/index.nginx-debian.html
+echo "HelloWorld from $(hostname -f)" | sudo tee /var/www/html/index.nginx-debian.html
 sudo systemctl enable nginx
 sudo systemctl start nginx 
